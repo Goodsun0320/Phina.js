e9535446b80a16dcbfe4a4f7765ffba9b74ce751
goodsun0320 <donmai2003shota@gmail.com>
2020-03-22 14:07:52 +0900

    敵追加

--- phinajs/main.js
+++ phinajs/main.js
@@ -50,6 +50,10 @@ var ASSETS = {
   
 };
 
+// グローバル変数
+var playerBulletGroup = null;
+var enemyBulletGroup = null;
+
 /*
  * メインシーン
  */
@@ -64,26 +68,87 @@ phina.define("MainScene", {
     this.backgroundColor = 'black';
     // グループ
     playerBulletGroup = DisplayElement().addChildTo(this);
+    enemyBulletGroup = DisplayElement().addChildTo(this);
     // プレイヤー
     Player().addChildTo(this).setPosition(this.gridX.center(), this.gridY.center(3));
-    // プレイヤーの弾作成
-    PlayerBullet().addChildTo(this).setPosition(this.gridX.center(), this.gridY.center());
+    // 敵
+    Enemy().addChildTo(this).setPosition(this.gridX.center(-3), this.gridY.center(-2));
+    Enemy().addChildTo(this).setPosition(this.gridX.center(3), this.gridY.center(-2));
   },
 });
+/*
+ * スペースシップクラス
+ */
+phina.define("SpaceShip", {
+  // 継承
+  superClass: 'Sprite',
+  // 初期化
+  init: function(param) {
+    // 親クラス初期化
+    this.superInit('spaceship', 64, 64);
+    // フレームアニメーションをアタッチ
+    this.anim = FrameAnimation('spaceship').attachTo(this);
+    // 移動スピード
+    this.speed = param.speed;
+  },
+  // 機体の移動
+  move: function(direction) {
+    this.moveBy(direction.x * this.speed, direction.y * this.speed);
+  },
+});
+
+phina.define("Collider", {
+  // 継承
+  superClass: 'RectangleShape',
+  // 初期化
+  init: function(param) {
+    this.superInit({
+      width: param.width,
+      height: param.height,
+      fill: null,
+      stroke: 'red',
+    });
+  },
+  // コライダーの絶対座標の矩形
+  getAbsoluteRect: function() {
+    var x = this.left + this.parent.x;
+    var y = this.top + this.parent.y;
+    return Rect(x, y, this.width, this.height);
+  },
+});
+
 /*
  * プレイヤークラス
  */
 phina.define("Player", {
   // 継承
-  superClass: 'Sprite',
+  superClass: 'SpaceShip',
   // 初期化
   init: function() {
     // 親クラス初期化
-    this.superInit('spaceship', 64, 64);
+    this.superInit({speed: 5 });
     // フレームアニメーションをアタッチ
-    FrameAnimation('spaceship').attachTo(this).gotoAndPlay('player');
-    // 移動スピード
-    this.speed = 5;
+    this.anim.gotoAndPlay('player');
+    // 当たり判定
+    this.collider = Collider({
+      width: 20,
+      height: 20,
+    }).addChildTo(this);
+
+    // 発射感覚
+    var shotDelay = 1000;
+    
+    // 一定間隔で弾を発射
+    this.tweener.clear()
+                .call(function() {
+                  this.shot();
+                }, this)
+                .wait(shotDelay)
+                .setLoop(true); 
+  },
+  // 弾を発射
+  shot: function() {
+    PlayerBullet().addChildTo(playerBulletGroup).setPosition(this.x, this.y);
   },
   // 毎フレーム更新処理
   update: function(app) {
@@ -93,6 +158,40 @@ phina.define("Player", {
     this.moveBy(direction.x * this.speed, direction.y * this.speed);
   },
 });
+
+/*
+ * 敵クラス
+ */
+phina.define("Enemy", {
+  // 継承
+  superClass: 'SpaceShip',
+  // 初期化
+  init: function() {
+    // 親クラス初期化
+    this.superInit({ speed: 0.5 });
+    // フレームアニメーション指定
+    this.anim.gotoAndPlay('enemy');
+    //
+    var shotDelay = 6000;
+    //
+    this.tweener.clear()
+                .call(function() {
+                  this.shot();
+                }, this)
+                .wait(shotDelay)
+                .setLoop(true);
+  },
+  // 弾発射
+  shot: function() {
+    EnemyBullet(this.rotation).addChildTo(enemyBulletGroup).setPosition(this.x, this.y);
+  },
+  // 毎フレーム更新処理
+  update: function() {
+    // 機体の移動
+    this.move(Vector2.DOWN);
+  },
+});
+
 /*
 * プレイヤーの弾クラス
 */
@@ -108,12 +207,38 @@ phina.define("PlayerBullet", {
    var self = this;
    // 左右の弾
    [-10, 10].each(function(dx) {
-    var playerbullet = Sprite('bullet', 64, 64).addChildTo(self).setPosition(self.x + dx, self.y);
-    playerbullet.frameIndex = 0;
+    Sprite('bullet', 64, 64).addChildTo(self).setPosition(self.x + dx, self.y).setFrameIndex(0);
    });
    // 上向き速度を与える
    this.physical.velocity.y = -speed;
  },
+});
+
+/*
+ * 敵の弾クラス
+ */
+phina.define("EnemyBullet", {
+  // 継承
+  superClass: 'DisplayElement',
+  // 初期化
+  init: function(rotation) {
+    // 親クラス初期化
+    this.superInit();
+    // スピード
+    var speed = 10;
+    var self = this;
+    // 弾
+    [-20, 0, 20].each(function(degree) {
+      // 弾作成
+      var bullet = Sprite('bullet', 64, 64).addChildTo(self).setFrameIndex(1);
+      // 発射方向を決める
+      var deg = rotation + degree + 90;
+      // 角度と大きさからベクトル作成
+      var vec = Vector2().fromDegree(deg, speed);
+      // ベクトルを代入
+      bullet.physical.velocity = vec;
+    });
+  },
 });
 
 /*
